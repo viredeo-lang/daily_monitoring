099433e93af02ece645f0b4fd25f5495fe3eb05e
viredeo-lang <viredeo@gmail.com>
2026-05-02 00:07:25 -0700

    Create service-worker.js with proper caching strategy.

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// Service Worker registration and caching strategy
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/script.js',
+                // Add more files you want to cache
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
